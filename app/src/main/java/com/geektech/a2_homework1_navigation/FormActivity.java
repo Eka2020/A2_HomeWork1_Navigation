fe0c9ad40a58555531091fa2ae244fab0b1f44de
Eka <eka_solnishko@mail.ru>
2020-05-01 03:32:29 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/a2_homework1_navigation/FormActivity.java
@@ -0,0 +1,49 @@
+package com.geektech.a2_homework1_navigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.geektech.a2_homework1_navigation.models.Task;
+
+public class FormActivity extends AppCompatActivity {
+    EditText editTitle;
+    EditText editDesc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Новая задача");
+        }
+        editTitle = findViewById(R.id.edit_title);
+        editDesc = findViewById(R.id.edit_desc);
+    }
+
+    public void onClick(View view) {
+        String title = editTitle.getText().toString().trim();
+        String desc = editDesc.getText().toString().trim();
+        Task task = new Task(title, desc);
+        Intent intent = new Intent();
+        intent.putExtra("task", task);
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+       if (id == R.id.action_HomeSecondFragment_to_HomeFragment) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/geektech/a2_homework1_navigation/ui/home/HomeSecondFragment.java
@@ -0,0 +1,6 @@
+package com.geektech.a2_homework1_navigation.ui.home;
+
+import android.app.Fragment;
+
+public class HomeSecondFragment extends Fragment {
+}
